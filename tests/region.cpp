6037700a61993c09c5ec20438eeb399f7ecee7e3
Rustam Subkhankulov <subkhankulov.rr@phystech.edu>
2025-10-15 18:08:53 +0500

    Added more iris::Region's API exfail tests.

--- tests/region.cpp
+++ tests/region.cpp
@@ -598,7 +598,7 @@ TEST(REGION, REPLACE_FINAL_BB_BY_ID) {
   EXPECT_EQ(region.getFinalBasicBlock(), newBBPtr);
 }
 
-TEST(REGION, DOM_INFO) {
+TEST(REGION, DOM_INFO_BASIC) {
   Region region("foo");
   EXPECT_TRUE(region.isDomInfoExpired());
 
@@ -648,6 +648,123 @@ TEST(REGION, EXFAIL_GET_RPO_NO_START_BB) {
     std::string str = exc.what();
     EXPECT_TRUE(
       !str.compare("Cannot run RPO with no start basic block specified!"));
+    return;
+  }
+  FAIL();
+}
+
+TEST(REGION, EXFAIL_GET_IDOM_EXPIRED_DOM_INFO) {
+  Region region("foo");
+
+  auto bb0 = std::make_unique<BasicBlock>(0);
+  auto bb0Ptr = bb0.get();
+  region.addStartBasicBlock(std::move(bb0));
+
+  region.collectDomInfo();
+  region.addBasicBlock(std::make_unique<BasicBlock>(1));
+
+  try {
+    [[maybe_unused]] auto idom = region.getIDom(bb0Ptr);
+  } catch (const IrisException& exc) {
+    std::string str = exc.what();
+    EXPECT_TRUE(!str.compare("Dom info is expired!"));
+    return;
+  }
+  FAIL();
+}
+
+TEST(REGION, EXFAIL_GET_IDOM_BY_ID_EXPIRED_DOM_INFO) {
+  Region region("foo");
+
+  auto bb0 = std::make_unique<BasicBlock>(0);
+  region.addStartBasicBlock(std::move(bb0));
+
+  region.collectDomInfo();
+  region.addBasicBlock(std::make_unique<BasicBlock>(1));
+
+  try {
+    [[maybe_unused]] auto idom = region.getIDomByID(0);
+  } catch (const IrisException& exc) {
+    std::string str = exc.what();
+    EXPECT_TRUE(!str.compare("Dom info is expired!"));
+    return;
+  }
+  FAIL();
+}
+
+TEST(REGION, EXFAIL_GET_DOM_BLOCKS_EXPIRED_DOM_INFO) {
+  Region region("foo");
+
+  auto bb0 = std::make_unique<BasicBlock>(0);
+  auto bb0Ptr = bb0.get();
+  region.addStartBasicBlock(std::move(bb0));
+
+  region.collectDomInfo();
+  region.addBasicBlock(std::make_unique<BasicBlock>(1));
+
+  try {
+    [[maybe_unused]] auto bbs = region.getDominatedBlocks(bb0Ptr);
+  } catch (const IrisException& exc) {
+    std::string str = exc.what();
+    EXPECT_TRUE(!str.compare("Dom info is expired!"));
+    return;
+  }
+  FAIL();
+}
+
+TEST(REGION, EXFAIL_GET_DOM_BLOCKS_BY_IDEXPIRED_DOM_INFO) {
+  Region region("foo");
+
+  auto bb0 = std::make_unique<BasicBlock>(0);
+  region.addStartBasicBlock(std::move(bb0));
+
+  region.collectDomInfo();
+  region.addBasicBlock(std::make_unique<BasicBlock>(1));
+
+  try {
+    [[maybe_unused]] auto bbs = region.getDominatedBlocksByID(0);
+  } catch (const IrisException& exc) {
+    std::string str = exc.what();
+    EXPECT_TRUE(!str.compare("Dom info is expired!"));
+    return;
+  }
+  FAIL();
+}
+
+TEST(REGION, EXFAIL_GET_DOM_CHAIN_EXPIRED_DOM_INFO) {
+  Region region("foo");
+
+  auto bb0 = std::make_unique<BasicBlock>(0);
+  auto bb0Ptr = bb0.get();
+  region.addStartBasicBlock(std::move(bb0));
+
+  region.collectDomInfo();
+  region.addBasicBlock(std::make_unique<BasicBlock>(1));
+
+  try {
+    [[maybe_unused]] auto chain = region.getDominatorsChain(bb0Ptr);
+  } catch (const IrisException& exc) {
+    std::string str = exc.what();
+    EXPECT_TRUE(!str.compare("Dom info is expired!"));
+    return;
+  }
+  FAIL();
+}
+
+TEST(REGION, EXFAIL_GET_DOM_CHAIN_BY_ID_EXPIRED_DOM_INFO) {
+  Region region("foo");
+
+  auto bb0 = std::make_unique<BasicBlock>(0);
+  region.addStartBasicBlock(std::move(bb0));
+
+  region.collectDomInfo();
+  region.addBasicBlock(std::make_unique<BasicBlock>(1));
+
+  try {
+    [[maybe_unused]] auto chain = region.getDominatorsChainByID(0);
+  } catch (const IrisException& exc) {
+    std::string str = exc.what();
+    EXPECT_TRUE(!str.compare("Dom info is expired!"));
     return;
   }
   FAIL();
